dab9b25a04635fd45814cd2ba91aa91848ab90a6
DarkJans19 <jansito4221@gmail.com>
2026-06-09 14:32:14 -0500

    refactor: Refactorized to made the code a little more efficient

--- src/bin_to_decimal.py
+++ src/bin_to_decimal.py
@@ -1,17 +1,16 @@
 def convert_binary_to_decimal(binary):
     if len(binary) > 8:
         raise ValueError("You can only convert 8 binary length")
-
-    result = 0
-    binary = binary[::-1]
     
     for i in range(len(binary)):
         if binary[i] != '1' and binary[i] != '0':
             raise ValueError("The binary number can only have 1 or 0")
-        
-        if binary[i] == '1':
-            print("binary:", binary[i])
-            result += pow(2, i)
+
+    result = 0
+
+    for bit in binary:
+        print(result)
+        result = result * 2 + int(bit)
 
     return result
 
